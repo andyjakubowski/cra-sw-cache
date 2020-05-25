14a330ef7d3f05faeb11a5243b5cc42513b8d6d4
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-25 18:24:56 +0200

    Updates

---
+++ precache-manifest.c97a61b75fc43fa5405fffbd7802ee8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f9e97d1abf7d38ac680d782f91d1f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "045e0b0bc08aa9fa6384",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "54500f2acd4b52ad4680",
+    "url": "/static/js/2.dab1e789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab1e789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "045e0b0bc08aa9fa6384",
+    "url": "/static/js/main.4327cbf3.chunk.js"
+  },
+  {
+    "revision": "4ffa86ddef329c993aba",
+    "url": "/static/js/runtime-main.3d21559c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
